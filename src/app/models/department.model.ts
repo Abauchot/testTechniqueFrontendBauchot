fd875d08b20165f468ba153654c352bf19b30fac
Antoine Bauchot <bchtantoine@gmail.com>
2025-11-18 13:45:38 +0100

    add department and region model interfaces

---
+++ src/app/models/department.model.ts
@@ -0,0 +1,6 @@
+export interface Department {
+    name: string;
+    code : string 
+    codeRegion : string;
+    _score : number;
+}
---
+++ src/app/models/region.model.ts
@@ -0,0 +1,5 @@
+export interface Region {
+    name: string;
+    code : string 
+    _score : number;
+}
